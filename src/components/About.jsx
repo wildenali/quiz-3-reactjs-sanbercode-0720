a63e728c1347e9842459f6490aea0369b0527a29
wildenali <wildenwildenaliali@gmail.com>
2020-08-09 12:30:39 +0700

    Isi data pribari di About page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,11 +8,11 @@ function About() {
           <div style={{ padding: "10px", border: "1px solid #ccc" }}>
             <h1 style={{ textAlign: "center" }}>Data Peserta Sanbercode Bootcamp Reactjs</h1>
             <ol>
-              <li><strong style={{ width: "100px" }}>Nama:</strong> nama peserta</li> 
-              <li><strong style={{ width: "100px" }}>Email:</strong> email pesera</li> 
-              <li><strong style={{ width: "100px" }}>Sistem Operasi yang digunakan:</strong> sistem operasi peserta</li>
-              <li><strong style={{ width: "100px" }}>Akun Gitlab:</strong> akun gitlab peserta</li> 
-              <li><strong style={{ width: "100px" }}>Akun Telegram:</strong> akun telegram peserta</li> 
+              <li><strong style={{ width: "100px" }}>Nama: </strong>Wilden Ali</li> 
+              <li><strong style={{ width: "100px" }}>Email: </strong>wildenwildenaliali@gmail.com</li> 
+              <li><strong style={{ width: "100px" }}>Sistem Operasi yang digunakan: </strong>Windows</li>
+              <li><strong style={{ width: "100px" }}>Akun Github: </strong>https://github.com/wildenali</li> 
+              <li><strong style={{ width: "100px" }}>Akun Telegram: </strong>@wildenali</li> 
             </ol>
           </div>
         </section>
