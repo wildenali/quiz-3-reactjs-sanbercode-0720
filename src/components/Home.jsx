245bda3ebcc321be48476c6794445ce2460a7de7
wildenali <wildenwildenaliali@gmail.com>
2020-08-09 12:32:59 +0700

    Menghapus import yg tidak dipakai

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,4 @@
-import React, { Component, useState, useEffect } from 'react'
-// import React, { useState, useEffect  } from 'react'
+import React, { Component } from 'react'
 import axios from 'axios'
 // import logo from '../assets/img/logo.png'
 
