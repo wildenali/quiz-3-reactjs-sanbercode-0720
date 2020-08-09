000827ccb9e723f467218ccabab36d378143820c
wildenali <wildenwildenaliali@gmail.com>
2020-08-09 12:16:44 +0700

    get data dari database dengan axios

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,33 @@
-import React, { Component } from 'react'
+import React, { Component, useState, useEffect } from 'react'
+// import React, { useState, useEffect  } from 'react'
+import axios from 'axios'
 // import logo from '../assets/img/logo.png'
 
 class Home extends Component {
   constructor(props){
     super(props)
-    this.state={
-      dataFilmTerbaik: [
-        { title: "Supermen", rating: 8, duration: 120, genre: "komedi", year: 2021, description: "lakilaki yang berkekuatan super power sekali dan sangan jago" },
-        { title: "Superwomen", rating: 9, duration: 90, genre: "crime", year: 2022, description: "perempuan yang berkekuatan super power jago sekali dan sangan elegant" }
-      ]
+    this.state = {
+      // dataFilmTerbaik: [
+      //   { title: "Supermen", rating: 8, duration: 120, genre: "komedi", year: 2021, description: "lakilaki yang berkekuatan super power sekali dan sangan jago" },
+      //   { title: "Superwomen", rating: 9, duration: 90, genre: "crime", year: 2022, description: "perempuan yang berkekuatan super power jago sekali dan sangan elegant" }
+      // ]
+      dataFilmTerbaik: [],
     }
+  }
+
+  getDataFilm = () => {
+    axios.get(`http://backendexample.sanbercloud.com/api/movies`)
+      .then(res => {
+        console.log(res)
+        console.log(res.data)
+        this.setState({ dataFilmTerbaik: res.data })
+        // setDataFilmTerbaik(res.data.map(el=>{return {id: el.id, title: el.title, rating:el.rating, duration:el.duration, genre:el.genre, year:el.year, description: el.description}}))
+      }
+    )
+  }
+
+  componentDidMount() {
+    this.getDataFilm()
   }
 
   render(){
@@ -24,7 +42,7 @@ class Home extends Component {
               {
                 this.state.dataFilmTerbaik.map((el, index) => {
                   return(
-                    <div>
+                    <div key={index}>
                       <a href=""><h3>{el.title}</h3></a>
                       <li style={{ fontWeight: "bold", listStyle: "none" }}>Rating: {el.rating}</li>
                       <li style={{ fontWeight: "bold", listStyle: "none" }}>Durasi: {el.duration} Menit</li>
